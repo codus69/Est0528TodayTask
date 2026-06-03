edb54698eab774c83c123fdc10b2493e27d67c76
이채연 <196144872+codus69@users.noreply.github.com>
2026-06-03 19:14:00 +0900

    feat:  index 리뷰 슬라이드 스와이퍼로 변경 html, js

--- js/main.js
+++ js/main.js
@@ -21,7 +21,7 @@ window.addEventListener("scroll", () => {
   }
 });
 
-const slideWrapper = document.querySelector(".slidewrapper"),
+/* const slideWrapper = document.querySelector(".slidewrapper"),
   slideContainer = slideWrapper.querySelector(".slidecontainer"),
   slides = slideContainer.querySelectorAll("li"),
   slideCount = slides.length,
@@ -29,33 +29,32 @@ const slideWrapper = document.querySelector(".slidewrapper"),
   nextBtn = slideWrapper.querySelector(".next");
 let currentIdx = 0;
 
-/* slideContainer 너비 지정 */
-slideContainer.style.width = `${slideCount * 100}%`;
+// slideContainer 너비 지정
+//slideContainer.style.width = `${slideCount * 100}%`;
 
-/* 
-슬라이드 이동 함수 
-moveSlide 함수 생성, 
-이 함수는 숫자 num이 들어오면 
-num 번호에 해당하는 슬라이드 보이도록 이동
-transform:translateX(33.3333%)
-*/
+
+//슬라이드 이동 함수 
+//moveSlide 함수 생성, 
+//이 함수는 숫자 num이 들어오면 
+//num 번호에 해당하는 슬라이드 보이도록 이동
+//transform:translateX(33.3333%)
+
 function moveSlide(num) {
   slideContainer.style.transform = `translateX(${-(num / slideCount) * 100}%)`;
   currentIdx = num;
 }
 
 //버튼으로 이동하기
-/*
-다음 버튼을 클릭하면 할일
-  변수명 nextIdx 다음 슬라이드 번호 생성
-  (마지막이면 첫번째 슬라이드로 이동)
-  moveSlide(nextIdx) 실행
+//다음 버튼을 클릭하면 할일
+  //변수명 nextIdx 다음 슬라이드 번호 생성
+  //(마지막이면 첫번째 슬라이드로 이동)
+  //moveSlide(nextIdx) 실행
 
-이전버튼을 클릭하면 할일
-  변수명 nextIdx 다음 슬라이드 번호 생성
-  (첫번째 슬라이드면 마지막 슬라이드로 이동)
-  moveSlide(nextIdx) 실행  
-*/
+//이전버튼을 클릭하면 할일
+  //변수명 nextIdx 다음 슬라이드 번호 생성
+  //(첫번째 슬라이드면 마지막 슬라이드로 이동)
+  //moveSlide(nextIdx) 실행  
+
 nextBtn.addEventListener("click", () => {
   let nextIdx = (currentIdx + 1) % slideCount;
   moveSlide(nextIdx);
@@ -63,4 +62,22 @@ nextBtn.addEventListener("click", () => {
 prevBtn.addEventListener("click", () => {
   let nextIdx = (currentIdx - 1 + slideCount) % slideCount;
   moveSlide(nextIdx);
+});
+*/
+
+// 1. 후기 슬라이드
+const reviewSwiper = new Swiper(".slidewrapper", {
+  wrapperClass: "slidecontainer",
+  slideClass: "slidecontainer > li",
+
+  loop: true,
+  slidesPerView: 1,
+
+  observer: true,
+  observeParents: true,
+
+  navigation: {
+    nextEl: ".slidewrapper .button.next",
+    prevEl: ".slidewrapper .button.prev",
+  },
 });
